a232c30400ae10c873bac5abfd21bdb31c9156d2
Pratik Gupte <pratikgupte16@gmail.com>
2022-08-16 15:59:38 +0100

    remove travis related files

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir docs
-cd docs
-wget https://gitlab.com/BlackEdder/rcourse/raw/master/RIntroduction.html
-wget https://gitlab.com/BlackEdder/rcourse/raw/master/RCoursePackage.html
-cp ../vignettes/*.html ./
-
